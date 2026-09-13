2a47c60020207d2232538a1674b24f6ad3e3803d
divyanshu <divyanshu473b@gmail.com>
2026-09-13 21:52:23 +0530

    Add logout route

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 const app = express();
+import cookieParser from "cookie-parser";
 
 import userRouter from "./routes/user.routes.js";
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/user", userRouter);
 
 export default app;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 const userRouter = Router();
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 import User from "../models/user.model.js";
 import {
@@ -31,8 +31,8 @@ userRouter.post("/signup", async (req, res) => {
       email: validationResult.data.email,
       password: hashedPass,
     });
-    const token = jwt.sign({id: newUser._id}, JWT_SECRET )
-    res.cookie("token", token)
+    const token = jwt.sign({ id: newUser._id }, JWT_SECRET);
+    res.cookie("token", token);
     res.status(201).json({
       success: true,
       message: "User signed up",
@@ -56,44 +56,62 @@ userRouter.post("/signup", async (req, res) => {
   }
 });
 userRouter.post("/signin", async (req, res) => {
-try {
-  const validationResult = signinSchema.safeParse(req.body);
-  if (!validationResult.success) {
-    const formattedErrors = validationResult.error.issues.map((e) => ({
-      path: e.path[0],
-      message: e.message,
-    }));
-    res.status(422).json({
-      success: false,
-      message: "Validation error",
-      error: formattedErrors,
+  try {
+    const validationResult = signinSchema.safeParse(req.body);
+    if (!validationResult.success) {
+      const formattedErrors = validationResult.error.issues.map((e) => ({
+        path: e.path[0],
+        message: e.message,
+      }));
+      res.status(422).json({
+        success: false,
+        message: "Validation error",
+        error: formattedErrors,
+      });
+      return;
+    }
+
+    const user = await User.findOne({ email: validationResult.data.email });
+    if (!user) {
+      res
+        .status(404)
+        .json({ success: false, message: "Wrong email or password" });
+      return;
+    }
+
+    const compareRes = await bcrypt.compare(
+      validationResult.data.password,
+      user.password,
+    );
+    if (!compareRes) {
+      res
+        .status(400)
+        .json({ success: false, message: "Wrong email or password" });
+      return;
+    }
+    const token = jwt.sign({ id: user._id }, JWT_SECRET);
+    res.cookie("token", token);
+    res.status(200).json({
+      success: true,
+      message: "User signed in",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
     });
-    return;
+  } catch (err) {
+    console.log("Signin Error: ", err);
+    res.status(500).json({ success: false, message: "Internal server error" });
   }
-
-  const user = await User.findOne({ email: validationResult.data.email });
-  if (!user) {
-    res
-      .status(404)
-      .json({ success: false, message: "Wrong email or password" });
-    return;
-  }
-
-  const compareRes = await bcrypt.compare(validationResult.data.password, user.password );
-  if(!compareRes) {
-    res.status(400).json({success: false, message: "Wrong email or password"})
-    return;
-  }
-  const token = jwt.sign({id: user._id}, JWT_SECRET)
-  res.cookie("token", token)
-  res.status(200).json({success: true, message: "User signed in", user: {
-    id: user._id,
-    name: user.name,
-    email: user.email,
-    }})
-} catch(err) {
-  console.log("Signin Error: ", err)
-  res.status(500).json({success: false, message: "Internal server error"})
+});
+userRouter.get("/logout", (req, res) => {
+  try {
+    res.clearCookie("token");
+    res.status(200).json({ success: true, message: "User logged out" });
+  } catch (err) {
+    console.log("Logout error: ", err);
+    res.send(500).json({ success: false, message: "Internal server error" });
   }
 });
 
